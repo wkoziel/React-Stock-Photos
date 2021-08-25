3190e4c685d22bdbb9f03d9d4d82c61e6257a245
Wojciech Kozieł <wojtek98koziel@gmail.com>
2021-08-25 16:59:26 +0200

    Data fetch works

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import { FaSearch } from 'react-icons/fa'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const mainURL = 'https://api.unsplash.com/photos'
+const searchURL = 'https://api.unsplash.com/search/photos'
+
+const App = () => {
+  const [photos, setPhotos] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  const fetchData = async () => {
+    try {
+      setLoading(true)
+      let url
+      url = `${mainURL}?client_id=fkmk3fliq4V-RXUf36ugG7OwA0kqGcN2Djz1I82uVy0`
+      const response = await fetch(url)
+      const data = await response.json()
+      setPhotos(data)
+      setLoading(false)
+    } catch (error) {
+      setLoading(false)
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  return <h1>App Component</h1>
 }
 
-export default App;
+export default App
